b7cb2b18378121ebd9c265a106d35f8342fbcb0d
Marc Taurasi <altairre.atsu@gmail.com>
2018-04-10 22:52:19 -0600

    Initial Commit

---
+++ Assets/EditorSnap.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class EditorSnap : MonoBehaviour {
+
+	[SerializeField]
+	[Range (10, 20)][Tooltip ("The Size of the grid squares in the world.")]
+	private int gridSize = 10;
+
+	void Update () {
+		Vector3 SnapPos;
+		SnapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+		SnapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+		SnapPos.y = 0f;
+
+		transform.position = SnapPos;
+	}
+}
