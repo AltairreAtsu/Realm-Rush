8e00137f1d1fc2489a032169025f81c2771dacaa
Marc Taurasi <altairre.atsu@gmail.com>
2018-04-11 13:19:30 -0600

    Imported Voxel assets. Made tower look at enemies

---
+++ Assets/Tower.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour {
+
+	[SerializeField]
+	private Transform turretTop;
+	[SerializeField]
+	private Transform target;
+
+	void Update () {
+		turretTop.LookAt(target);
+	}
+}
