83bb9193e2e1e2323c1514dff897eb664e801e08
Marc Taurasi <altairre.atsu@gmail.com>
2018-04-10 23:18:32 -0600

    Edited Editor Snap script. made new label that automatically updates co-ords display.

--- Assets/CubeEditor.cs
+++ Assets/CubeEditor.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
-public class EditorSnap : MonoBehaviour {
+[SelectionBase]
+public class CubeEditor : MonoBehaviour {
 
 	[SerializeField]
 	[Range (10, 20)][Tooltip ("The Size of the grid squares in the world.")]
 	private int gridSize = 10;
 
+	private TextMesh textMesh;
+
+	private void Start()
+	{
+		textMesh = GetComponentInChildren<TextMesh>();
+	}
+
 	void Update () {
+		if (!textMesh)
+		{
+			textMesh = GetComponentInChildren<TextMesh>();
+		}
+
 		Vector3 SnapPos;
+
 		SnapPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
 		SnapPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
 		SnapPos.y = 0f;
 
 		transform.position = SnapPos;
+		textMesh.text = SnapPos.x/gridSize + ", " + SnapPos.z/gridSize;
 	}
 }
