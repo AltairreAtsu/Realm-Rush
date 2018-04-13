66fdc5663f525f7120f0ec935122d8ab3770777f
Marc Taurasi <altairre.atsu@gmail.com>
2018-04-12 22:54:38 -0600

    Added health variable to the base prefab.

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour {
+
+	[SerializeField] private int health = 10;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		health--;
+		if(health <= 0)
+		{
+			// Loose the game
+		}
+	}
+}
--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour {
 	private Pathfinder pathfinder;
 
-	[SerializeField]
-	private int hits = 10;
+	[SerializeField] private int hitPoints = 10;
+	[SerializeField] private float movementPeriod = .5f;
 
 	void Start ()
 	{
@@ -19,26 +19,27 @@ public class EnemyMovement : MonoBehaviour {
 		foreach (Waypoint waypoint in path)
 		{
 			transform.position = waypoint.transform.position;
-			yield return new WaitForSeconds(1f);
+			yield return new WaitForSeconds(movementPeriod);
 		}
+		Die();
 	}
 
 	private void OnParticleCollision(GameObject other)
 	{
 		Damage();
-		if(hits <= 0)
+		if(hitPoints <= 0)
 		{
-			Kill();
+			Die();
 		}
 	}
 
 	private void Damage()
 	{
-		hits--;
+		hitPoints--;
 		// TODO: Hit Sounds and Particles
 	}
 
-	private void Kill()
+	private void Die()
 	{
 		Destroy(gameObject); // TODO: sound effect and particle system for enemy deaths?
 	}
